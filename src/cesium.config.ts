dcfd3273a1c6e2b50a52765fc7e5fc47f0121ac5
18023785187 <1069532844@qq.com>
2022-01-31 01:12:42 +0800

    2022/1/31 update: 修改cesium资源路径

--- src/cesium.config.ts
+++ src/cesium.config.ts
@@ -1,7 +1,7 @@
 import { Ion } from 'cesium'
 import { ASSETS_TOKEN } from '@/constants'
 
-window.CESIUM_BASE_URL = process!.env!.NODE_ENV === 'development' ? __dirname : __dirname + 'cesium_study/dist'
+window.CESIUM_BASE_URL = process!.env!.NODE_ENV === 'development' ? __dirname : __dirname + ''
 
 // 设置令牌
 Ion.defaultAccessToken = ASSETS_TOKEN
